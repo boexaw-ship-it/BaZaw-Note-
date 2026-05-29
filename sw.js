4e3ef7a66161fb296d47e5111197088227067328
zawzaw <boexaw@gmail.com>
2026-05-29 20:47:49 +0630

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='bazaw-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./css/style.css']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
